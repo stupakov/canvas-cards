8a4655e99b53f49ee2f077f1f215398e5a1631e5
Alex Stupakov <stupakov@gmail.com>
2020-04-03 20:18:50 -0700

    use hash router so that the app can be deployed to github pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import CanvasCard from './CanvasCard.js'
 import AnimatedCard from './AnimatedCard.js'
 import AnimatedPage from './AnimatedPage.js'
