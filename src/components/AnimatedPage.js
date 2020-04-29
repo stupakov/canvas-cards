67c0de87ad6641b10b508082082a31dfb579815b
Alex Stupakov <stupakov@gmail.com>
2020-04-28 20:15:44 -0700

    add font and logo to Geo AnimatedPage

--- src/components/AnimatedPage.js
+++ src/components/AnimatedPage.js
@@ -3,13 +3,37 @@ import { prepareCanvas } from '../common/drawing.js'
 import { initTimeCounter } from '../common/animation.js'
 import { gsap } from 'gsap'
 
+import Logo from '../images/logo1.svg'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='logo'>
+        <img src={Logo} alt='Logo' />
+        <div className='logo-text'>Geometer</div>
+      </div>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className='footer-left'>+1-415-555-GEOS</div>
+      <div className='footer-right'>
+        <a href='mailto:hello@geometer.io'>hello@geometer.io</a>
+      </div>
+    </div>
+  )
+}
+
 const AnimatedPage = ({ animations, name }) => {
   useEffect(() => {
     gsap.ticker.fps(30)
 
     let canvas = document.getElementById(name)
     let width, height, state, context
-    
+
     const getCurrentTime = initTimeCounter()
 
     const prepareAnimation = () => {
@@ -34,7 +58,13 @@ const AnimatedPage = ({ animations, name }) => {
     })
   })
 
-  return <canvas id={name} data-name={name}></canvas>
+  return (
+    <div className='page-container'>
+      <Header />
+      <Footer />
+      <canvas id={name} data-name={name}></canvas>
+    </div>
+  )
 }
 
 export default AnimatedPage
